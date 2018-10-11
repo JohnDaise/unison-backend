abac6a6503984e1c195f6a6f650c987aa8532109
JohnDaise <jdaise@gmail.com>
2018-10-11 12:04:01 -0400

    basic backend setup for user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+
+
+
+
+private
+
+   def user_params
+     params.permit(:name, :email, :phone_number, :bio, :pic_url)
+   end
+
+   def find_user
+     @user= User.find(params[:id])
+   end
+
 end
